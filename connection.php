b7fedb9c1907ba6a5989fe3c23a4fd1c92e4bca7
shahabudddin khan <153116922+shahabuddinaziz786@users.noreply.github.com>
2024-09-15 23:23:59 +0530

    connection in database

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$conn = mysqli_connect('localhost','root','','user_manage');
+// if($conn){
+//     echo "connection successful";
+// }else{
+//     echo "Connection failed" . mysqli_error($conn);
+
+// }
+
+?>
